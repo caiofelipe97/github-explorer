24aa4e8bd765f9e656eccd02001603bc44d4a724
Caio Melo <caiofelipee17@gmail.com>
2020-05-22 19:33:53 -0300

    Add Dashboard style

--- src/pages/Dashbboard/index.tsx
+++ src/pages/Dashbboard/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Gitghub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositorios no Gitghub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/20324935?s=460&u=252e93639dae9e6e666547a2856144fc3fea3fcb&v=4"
+            alt="Caio Felipe"
+          />
+          <div>
+            <strong>github-explorer</strong>
+            <p>Project to explore Github projects and issues made in ReactJS using Typescript</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/20324935?s=460&u=252e93639dae9e6e666547a2856144fc3fea3fcb&v=4"
+            alt="Caio Felipe"
+          />
+          <div>
+            <strong>github-explorer</strong>
+            <p>Project to explore Github projects and issues made in ReactJS using Typescript</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/20324935?s=460&u=252e93639dae9e6e666547a2856144fc3fea3fcb&v=4"
+            alt="Caio Felipe"
+          />
+          <div>
+            <strong>github-explorer</strong>
+            <p>Project to explore Github projects and issues made in ReactJS using Typescript</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
